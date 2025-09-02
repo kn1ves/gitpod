e9fdb6683679fae6fddcbce6e752aed7a05ff604
kn1ves <cross399@gmail.com>
2025-09-02 18:33:48 +0530

    feat: Update config

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 # Install Neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
-# Install NvChad
-git clone https://github.com/NvChad/starter ~/.config/nvim
+# Install custom config
+git clone https://github.com/kn1ves/nvim ~/.config/nvim
+git checkout feat/kickstart
 
 # Cleanup
-rm -f nvim-linux64.tar.gz
-
+rm -f nvim-linux-x86_64.tar.gz
 # Update PATH
-export PATH="$PATH:/opt/nvim-linux64/bin"
+export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
 
 # Make PATH persistent
-if ! grep -q "/opt/nvim-linux64/bin" ~/.zshrc; then
-    echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.zshrc
+if ! grep -q "/opt/nvim-linux-x86_64/bin" ~/.zshrc; then
+    echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.zshrc
 fi
